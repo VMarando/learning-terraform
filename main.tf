1df0f22a89f5a6812e546326f54d6b67602c85a4
dnemtsanov <dnemtsanov@gmail.com>
2025-01-19 18:21:49 -0500

    Load Balancer AWS 2

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_vpc" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog_alb"
+  name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
