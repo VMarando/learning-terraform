2934f3f361f9487e7df32913d02b5e5c1c4afe7f
bhendega <39784106+bhendega@users.noreply.github.com>
2025-01-19 19:17:44 -0800

    datatype

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ data "aws_ami" "app_ami" {
 }
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "Learning Terraform"
+    Name = var.instance_type
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
  variable "instance_type" {
   description = "Type of EC2 instance to provision"
+  type        = string
   default     = "t3.nano"
 }
