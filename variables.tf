0ab575e7def51c905fef66c02aa12f3c4b4a4a14
bhendega <39784106+bhendega@users.noreply.github.com>
2025-01-19 19:21:52 -0800

    variableaddedregion

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
  variable "instance_type" {
   description = "Type of EC2 instance to provision"
   type        = string
