aa9470c8cb386a2211d0a460751c3caddf3f2f9f
bhendega <39784106+bhendega@users.noreply.github.com>
2025-01-20 16:33:13 -0800

    test-001

--- variables.tf
+++ variables.tf
@@ -1,26 +1,5 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
-}
-
  variable "instance_type" {
   description = "Type of EC2 instance to provision"
   type        = string
   default     = "t3.nano"
-}
-
-variable "rdms_name" {
-  type    = string
-  default = "mariadb_10.6"
-}
-
-variable "service_name" {
-  type    = string
-  default = "forum"
-}
-
-variable "environment" {
-  type    = string
-  default = "dev"
 }
