0fb12121028c95965fad8f71d050baf071462513
RMJcar <43453538+RMJcar@users.noreply.github.com>
2025-01-29 10:40:54 -0500

    switched name to network

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = "${var.environment.network_prefix}${var.environment.name_prefix}/16"
+  cidr = "${var.environment.network_prefix}${var.environment.network_prefix}/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["${var.environment.name_prefix}.101.0/24", "${var.environment.name_prefix}.102.0/24", "${var.environment.name_prefix}.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
