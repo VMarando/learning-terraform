724f0e4360f78b16194ed00675962ee904e9fb3f
RMJcar <43453538+RMJcar@users.noreply.github.com>
2025-01-29 10:36:24 -0500

    cleaned up variables

--- variables.tf
+++ variables.tf
@@ -40,18 +40,3 @@ variable "asg_max_size" {
   description = "Maximum number of instance in the ASG"
   default     = 2
 }
-   
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = 
-
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
