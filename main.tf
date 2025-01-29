1cb791a37a09167a9f7a836d75f6c4eac15a9059
RMJcar <43453538+RMJcar@users.noreply.github.com>
2025-01-29 10:46:48 -0500

    work!

--- main.tf
+++ main.tf
@@ -83,10 +83,10 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "${var.environment.name}-blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["${var.environment.network_prefix}/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["${var.environment.network_prefix}/0"]
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "${var.environment.name}-blog"
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["${var.environment.network_prefix}.0.0/0"]
+  egress_rules.       = ["all-all"]
+  egress_cidr_blocks  = ["${var.environment.network_prefix}.0.0/0"]
 }
